1f477ae36a9acbee62ae06e2dffc31ca31e66b7d
lostqubit <sg_suryaprakash@yahoo.com>
2024-05-24 12:08:40 +0530

    render current weather

--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
+import {addSeconds} from "date-fns"
+
 const dataLoader = (() => {
     const API_KEY = "84946dd5efaa6549cfd389a8db1f602b";
 
     const getCurrentForecast = async (cityName,units="metric") => {
-        const forecast = [];
         let error;
+        let currentWeather;
+        const forecast = [];
 
         try{
             const response1 = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`);
@@ -16,28 +19,51 @@ const dataLoader = (() => {
     
             const lat = cityDetails[0].lat;
             const lon = cityDetails[0].lon;
-    
-            const response2 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=${units}`);
-            const data = await response2.json();
+            
+            const response2 = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+            const currentData = await response2.json();
+
+            if(!currentData.weather.length){
+                console.log("Something went wrong");
+                return null;
+            }
+
+            const response3 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=${units}`);
+            const data = await response3.json();
     
             if(!data.cnt){
-                console.log("Something went wrong")
+                console.log("Something went wrong");
                 return null;
             }
     
             const city = data.city.name;
             const country = data.city.country;
             const timeOffset = data.city.timezone;
+
+            const currentTime = new Date(currentData.dt*1000);
+
+            currentWeather = {
+                city: city,
+                country: country,
+                timestamp: addSeconds(currentTime,currentTime.getTimezoneOffset()*60+timeOffset),
+                temperature: currentData.main,
+                weather: currentData.weather[0],
+                wind: currentData.wind,
+                pop: currentData.pop,
+                visibility: currentData.visibility,
+            };
     
             for(let dataItem of data.list){
+                const time = new Date(dataItem.dt*1000);
                 forecast.push({
                     city: city,
                     country: country,
-                    timestamp: new Date((dataItem.dt + timeOffset)*1000),
+                    timestamp: addSeconds(time,time.getTimezoneOffset()*60+timeOffset),
                     temperature: dataItem.main,
                     weather: dataItem.weather[0],
                     wind: dataItem.wind,
                     pop: dataItem.pop,
+                    visibility: dataItem.visibility,
                 });
             }
 
@@ -47,7 +73,7 @@ const dataLoader = (() => {
             error = e;
         }
 
-        return {error,forecast};
+        return {error,currentWeather,forecast};
     };
 
     return {getCurrentForecast};
--- src/render.js
+++ src/render.js
@@ -1,4 +1,5 @@
 import dataLoader from './data.js';
+import reloadIcon from './icons/reload.svg';
 
 const display = (() => {
     const container = document.querySelector("#content-body");
@@ -14,30 +15,187 @@ const display = (() => {
         document.querySelector(".loader").remove();
     }
 
+    const renderCurrentWeather = (currentWeather,currentPop) => {
+        const wrapperDiv = document.createElement("div");
+        wrapperDiv.classList.add("current-card");
+
+        const card = document.createElement("div");
+        card.classList.add("card-contents");
+        
+        const div1 = document.createElement("div");
+        const div2 = document.createElement("div");
+        const h4 = document.createElement("h4");
+        h4.innerText = "Current weather";
+        const time = document.createElement("p");
+        const currentTime = currentWeather.timestamp.split(" ")[4].split(":").slice(0,2);
+        time.innerText = currentTime[0]+":"+currentTime[1];
+        div2.appendChild(h4);
+        div2.appendChild(time);
+        const reloadButton = document.createElement("img");
+        reloadButton.id = "reload-btn";
+        reloadButton.src = reloadIcon;
+        div1.appendChild(div2);
+        div1.appendChild(reloadButton)
+        card.appendChild(div1);
+
+        const div3 = document.createElement("div");
+        div3.id = "current-temperature";
+        const currentIcon = document.createElement("img");
+        currentIcon.id = "currentWeather-icon";
+        currentIcon.src = `https://openweathermap.org/img/wn/${currentWeather.weather.icon}@2x.png`
+        const div4 = document.createElement("div");
+        const h1 = document.createElement("h1");
+        //h1.innerText = currentWeather.main.temp;
+        h1.innerText = currentWeather.temperature.temp;
+        const h2 = document.createElement("h2");
+        h2.innerText = "\u00B0C";
+        const div5 = document.createElement("div");
+        const weather = document.createElement("h3");
+        weather.innerText = currentWeather.weather.main[0].toUpperCase() + currentWeather.weather.main.slice(1);
+        const feelsLike = document.createElement("p");
+        //feelsLike.innerText = `Feels like ${currentWeather.main.feels_like}`;
+        feelsLike.innerText = `Feels like ${currentWeather.temperature.feels_like}\u00B0`;
+        div5.appendChild(weather);
+        div5.appendChild(feelsLike);
+        div4.appendChild(h1);
+        div4.appendChild(h2);
+        div3.appendChild(currentIcon);
+        div3.appendChild(div4);
+        div3.appendChild(div5);
+        card.appendChild(div3);
+
+        const description = document.createElement("p");
+        description.id = "description";
+        description.innerText = currentWeather.weather.description[0].toUpperCase() + currentWeather.weather.description.slice(1);
+        description.innerText += `. The Low will be ${currentWeather.temperature.temp_min}\u00B0C`
+        card.appendChild(description)
+
+        const dataDiv = document.createElement("div");
+        dataDiv.id = "data";
+
+        const windSpeedDiv = document.createElement("div");
+        const windSpeedHeading = document.createElement("h4")
+        windSpeedHeading.innerText = "Wind"
+        const windSpeed = document.createElement("p");
+        windSpeed.innerText = `${currentWeather.wind.speed} m/s`;
+        windSpeedDiv.appendChild(windSpeedHeading);
+        windSpeedDiv.appendChild(windSpeed);
+        dataDiv.appendChild(windSpeedDiv);
+
+        const humidityDiv = document.createElement("div");
+        const humidityHeading = document.createElement("h4");
+        humidityHeading.innerText = "Humidity";
+        const humidity = document.createElement("p");
+        humidity.innerText = `${currentWeather.temperature.humidity}%`;
+        humidityDiv.appendChild(humidityHeading);
+        humidityDiv.appendChild(humidity);
+        dataDiv.appendChild(humidityDiv);
+
+        const visibilityDiv = document.createElement("div");
+        const visibilityHeading = document.createElement("h4");
+        visibilityHeading.innerText = "Visibility";
+        const visibility = document.createElement("p");
+        visibility.innerText = `${currentWeather.visibility/1000} km`;
+        visibilityDiv.appendChild(visibilityHeading);
+        visibilityDiv.appendChild(visibility);
+        dataDiv.appendChild(visibilityDiv);
+
+        const pressureDiv = document.createElement("div");
+        const pressureHeading = document.createElement("h4");
+        pressureHeading.innerText = "Pressure";
+        const pressure = document.createElement("p");
+        pressure.innerText = `${currentWeather.temperature.pressure} mb`;
+        pressureDiv.appendChild(pressureHeading);
+        pressureDiv.appendChild(pressure);
+        dataDiv.appendChild(pressureDiv);
+
+        const popDiv = document.createElement("div");
+        const popHeading = document.createElement("h4");
+        popHeading.innerText = "Chance of rain";
+        const pop = document.createElement("p");
+        pop.innerText = `${currentPop*100}%`;
+        popDiv.appendChild(popHeading);
+        popDiv.appendChild(pop);
+        dataDiv.appendChild(popDiv);
+
+        card.appendChild(dataDiv);
+        wrapperDiv.appendChild(card);
+
+        return wrapperDiv;
+    }
+
+    const renderForecast = (currentWeather, forecasts) => {
+        const currentPop = forecasts[0].pop;
+
+        const heading = document.createElement("h3");
+        heading.innerText = `${forecasts[0].city}, ${forecasts[0].country}`
+        
+        const currentContent = renderCurrentWeather(currentWeather,currentPop);
+
+        return {heading,currentContent};
+    };
+
     const init = async () => {
         loadAnimation();
         const form = document.querySelector("#search");
         form.addEventListener("submit", async (event) => {
             event.preventDefault();
+            loadAnimation();
             const formdata = new FormData(event.target);
             const query = formdata.get("search-query");
             const data = await dataLoader.getCurrentForecast(query);
+            stopAnimation();
             if(data.error){
                 console.log("API down! Please Try Again");
             }
             else{
-                const forecasts = data.forecast;
-                console.log(forecasts);
+                const {heading,currentContent} = renderForecast(data.currentWeather, data.forecast);
+                container.appendChild(heading);
+                container.appendChild(currentContent);
             }
+            form.reset();
         });
-        // const coordinates = await dataLoader.getCurrentForecast("New Delhi");
-        // console.log(coordinates);
+        // const data = await dataLoader.getCurrentForecast("New Delhi");
+
+        const test = {
+            timestamp: "Thu May 23 2024 20:30:00 GMT+0530 (India Standard Time) {}",
+            city: "New Delhi",
+            country: "IN",
+            pop: 0.24,
+            visibility: 10000,
+            temperature: {
+                feels_like: 31.48,
+                grnd_level: 991,
+                humidity: 80,
+                pressure: 1003,
+                sea_level: 1003,
+                temp: 27.75,
+                temp_kf: 0,
+                temp_max: 27.75,
+                temp_min: 27.75,
+            },
+            weather:{
+                description: "overcast clouds",
+                icon: "04n",
+                id: 804,
+                main: "Clouds",
+            } ,
+            wind:{
+                deg: 220,
+                gust: 4.27,
+                speed: 2.78,
+            },
+        }
+
+        const testForecast = [];
+        for(let i=0;i<40;i++) testForecast.push(test);
+
+        const {heading,currentContent} = renderForecast(test, testForecast);
+        
         stopAnimation();
+        container.appendChild(heading);
+        container.appendChild(currentContent);
     }
-
-    const renderForecast = () => {
-
-    };
 
     return {init};
 })();
